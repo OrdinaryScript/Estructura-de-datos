54a988f830f9f5e1cdd3fe77ed77cdae41b7fd4f
OrdinaryScript <edy2cg@hotmail.com>
2021-05-10 20:57:48 -0700

    Update Prefijo_infijo_postfijo.cs

--- Estructuras no lineales/Prefijo_infijo_postfijo.cs
+++ Estructuras no lineales/Prefijo_infijo_postfijo.cs
@@ -4,10 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Campos Guillen Eduardo
-// Ing. Sistemas Computacionales
-// Estructura de Datos
-
 namespace Tres_recorridos
 {
     class Program
